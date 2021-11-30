0743a7988ca290719b4bef634a4b7e2126b5b0a8
mohamedmbh <mohamedbenhamzaa@gmail.com>
2021-11-30 14:12:28 +0100

    Modifica la cantidad de insercion de números

--- src/mainApp/MainApp.java
+++ src/mainApp/MainApp.java
@@ -2,14 +2,14 @@ package mainApp;
 
 public class MainApp {
 
-	private static int[] array = new int[20];
+	private static int[] array = new int[30];
 
 	public static void main(String[] args) {
 
 	}
 
 	public static void insertarNumeros() {
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 30; i++) {
 			int n = (int) Math.random() * (10 + 1);
 			array[i] = n;
 		}
