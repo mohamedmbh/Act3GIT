e15fb331c46b4b3c282e48b741527ace6de2b923
mohamedmbh <mohamedbenhamzaa@gmail.com>
2021-11-30 13:53:33 +0100

    Añade verContenidoArray() a rama1

--- src/mainApp/MainApp.java
+++ src/mainApp/MainApp.java
@@ -3,17 +3,21 @@ package mainApp;
 public class MainApp {
 
 	private static int[] array = new int[20];
-	
+
 	public static void main(String[] args) {
 
-		
-		
 	}
 
 	public static void insertarNumeros() {
 		for (int i = 0; i < 20; i++) {
 			int n = (int) Math.random() * (10 + 1);
 			array[i] = n;
+		}
+	}
+
+	public static void verContenidoArray() {
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
 		}
 	}
 }
