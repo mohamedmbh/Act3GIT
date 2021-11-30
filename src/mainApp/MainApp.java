526266c40596f767d9157f4dc2aadb5dfb326d15
mohamedmbh <mohamedbenhamzaa@gmail.com>
2021-11-30 12:45:26 +0100

    Añade insertarNumeros()

--- src/mainApp/MainApp.java
+++ src/mainApp/MainApp.java
@@ -2,11 +2,18 @@ package mainApp;
 
 public class MainApp {
 
+	private static int[] array = new int[20];
+	
 	public static void main(String[] args) {
 
 		
 		
 	}
 
-	
+	public static void insertarNumeros() {
+		for (int i = 0; i < 20; i++) {
+			int n = (int) Math.random() * (10 + 1);
+			array[i] = n;
+		}
+	}
 }
